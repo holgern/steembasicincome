13ef090842d82ca41a2d606b8fa78fadeeea7dd3
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2018-12-22 22:24:20 +0000

    Remove unused steembasicincome_ops in which account history ops were stored by index

--- sbi_store_ops_db.py
+++ sbi_store_ops_db.py
@@ -48,51 +48,14 @@ if __name__ == "__main__":
     
     accountTrx = {}
     for account in accounts:
-        accountTrx[account] = AccountTrx(db, account)
-        if not accountTrx[account].exists_table():
-            accountTrx[account].create_table()
-    # temp
-    accountTrx["sbi"] = AccountTrx(db, "sbi")
+        if account == "steembasicincome":
+            accountTrx["sbi"] = AccountTrx(db, "sbi")
+        else:
+            accountTrx[account] = AccountTrx(db, account)
 
     # stop_index = addTzInfo(datetime(2018, 7, 21, 23, 46, 00))
     # stop_index = formatTimeString("2018-07-21T23:46:09")
     
-    for account_name in accounts:
-        if account_name != "steembasicincome":
-            continue        
-        account = Account(account_name, steem_instance=stm)
-        
-        # Go trough all transfer ops
-        cnt = 0
-
-        start_index = accountTrx[account_name].get_latest_index()
-        if start_index is not None:
-            start_index = start_index["op_acc_index"] + 1
-            print("account %s - %d" % (account["name"], start_index))
-        else:
-            start_index = 0
-        data = []
-        if account.virtual_op_count() > start_index:
-            for op in account.history(start=start_index, use_block_num=False):
-                virtual_op = op["virtual_op"]
-                trx_in_block = op["trx_in_block"]
-                if virtual_op > 0:
-                    trx_in_block = -1
-                d = {"block": op["block"], "op_acc_index": op["index"], "op_acc_name": account["name"], "trx_in_block": trx_in_block,
-                     "op_in_trx": op["op_in_trx"], "virtual_op": virtual_op,  "timestamp": formatTimeString(op["timestamp"]), "type": op["type"], "op_dict": json.dumps(op)}
-                data.append(d)
-                if cnt % 1000 == 0:
-                    print(op["timestamp"])
-                    accountTrx[account_name].add_batch(data)
-                    data = []
-                cnt += 1
-            if len(data) > 0:
-                print(op["timestamp"])
-                accountTrx[account_name].add_batch(data)
-                data = []            
-    
-    # start sbi2-sbi10
-
     for account_name in accounts:
         if account_name == "steembasicincome":
             account = Account(account_name, steem_instance=stm)
