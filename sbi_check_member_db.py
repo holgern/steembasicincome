75da73eaac2b0e040a18ffc95485f7a1dd026167
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2018-12-30 12:31:32 +0000

    Check for wrong account names in sbi_check_member_db.py

--- sbi_check_member_db.py
+++ sbi_check_member_db.py
@@ -11,7 +11,7 @@ from time import sleep
 import dataset
 import json
 from steembi.parse_hist_op import ParseAccountHist
-from steembi.storage import TrxDB, MemberDB, ConfigurationDB
+from steembi.storage import TrxDB, MemberDB, ConfigurationDB, AccountsDB
 
 
 if __name__ == "__main__":
@@ -21,11 +21,8 @@ if __name__ == "__main__":
     else:
         with open(config_file) as json_data_file:
             config_data = json.load(json_data_file)
-        print(config_data)
-        accounts = config_data["accounts"]
         databaseConnector = config_data["databaseConnector"]
         databaseConnector2 = config_data["databaseConnector2"]
-        other_accounts = config_data["other_accounts"]
         mgnt_shares = config_data["mgnt_shares"]
 
     db2 = dataset.connect(databaseConnector2)
@@ -34,22 +31,19 @@ if __name__ == "__main__":
     memberStorage = MemberDB(db2)
     confStorage = ConfigurationDB(db2)
     
+    accStorage = AccountsDB(db2)
+    accounts = accStorage.get()
+    other_accounts = accStorage.get_transfer()
+    
+    sp_share_ratio = confStorage.get()["sp_share_ratio"]
+    
     nodes = NodeList()
     try:
         nodes.update_nodes()
     except:
         print("could not update nodes")    
     stm = Steem(node=nodes.get_nodes())    
-    
-    newTrxStorage = False
-    if not trxStorage.exists_table():
-        newTrxStorage = True
-        trxStorage.create_table()
-    
-    newMemberStorage = False
-    if not memberStorage.exists_table():
-        newMemberStorage = True
-        memberStorage.create_table()
+
 
     # Update current node list from @fullnodeupdate
     print("check member database")
@@ -57,31 +51,46 @@ if __name__ == "__main__":
     member_accounts = memberStorage.get_all_accounts()
     data = trxStorage.get_all_data()
     
+    missing_accounts = []
     member_data = {}
+    aborted = False
     for m in member_accounts:
         member_data[m] = memberStorage.get(m)
+    # Check wrong account names:
+    if False:
+        cnt = 0
+        for m in member_accounts:
+            if aborted:
+                continue
+            cnt += 1
+            if cnt % 100 == 0:
+                print("%d/%d scanned" % ())
+            try:
+                acc = Account(m, steem_instance=stm)
+            except KeyboardInterrupt:
+                aborted = True
+            except:
+                print("%s is not a valid account" % m)
+                missing_accounts.append(m)
     
-    for op in data:
-        if op["status"].lower() == "valid":
-            share_type = op["share_type"]
-            if share_type.lower() in ["delegation"]:
-                if op["shares"] > 0 and op["sponsor"] in member_data:
-                    # print("del. bonus_shares: %s - %d" % (op["sponsor"], op["shares"]))
-                    member_data[op["sponsor"]]["bonus_shares"] += op["shares"]
-                elif op["vests"] > 0 and op["sponsor"] in member_data:
-                    sp = stm.vests_to_sp(float(op["vests"]))
-                    member_data[op["sponsor"]]["bonus_shares"] += int(sp / confStorage.get()["sp_share_ratio"])
-            elif share_type.lower() in ["mgmt", "mgmttransfer"]:
-                if op["shares"] > 0 and op["sponsor"] in member_data:
-                    member_data[op["sponsor"]]["bonus_shares"] += op["shares"]
-                    # print("mngt bonus_shares: %s - %d" % (op["sponsor"], op["shares"]))
+
 
     shares = 0
     bonus_shares = 0
+    balance_rshares = 0
     for m in member_data:
         shares += member_data[m]["shares"]
         bonus_shares += member_data[m]["bonus_shares"]
-    print("shares: %d" % shares)
-    print("bonus shares: %d" % bonus_shares)
-    print("total shares: %d" % (shares + bonus_shares))
+        balance_rshares += member_data[m]["balance_rshares"]
+    
+    
+    print("units: %d" % shares)
+    print("bonus units: %d" % bonus_shares)
+    print("total units: %d" % (shares + bonus_shares))
+    print("----------")
+    print("balance_rshares: %d" % balance_rshares)
+    print("balance_rshares: %.3f $" % stm.rshares_to_sbd(balance_rshares))
+    if len(missing_accounts) > 0:
+        print("%d not existing accounts: " % len(missing_accounts))
+        print(missing_accounts)
     
