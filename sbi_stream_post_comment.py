2dbf9b354c439199578e97138551d105a0a049ff
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2020-03-23 17:20:09 +0000

    fix typo

--- sbi_stream_post_comment.py
+++ sbi_stream_post_comment.py
@@ -187,7 +187,7 @@ if __name__ == "__main__":
         if ops["author"] not in changed_member_data:
             changed_member_data.append(ops["author"])
         if main_post:
-            if c["created"] == c["last_update] :
+            if c["created"] == c["last_update"]:
                 member_data[ops["author"]]["last_post"] = c["created"]
                 member_data[ops["author"]]["comment_upvote"] = 0
         else:
